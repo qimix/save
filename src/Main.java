b99f673d76fcda773d942d822721566823855d96
Vitaliy Murzin <murzinvit@yandex.ru>
2024-04-04 13:26:15 +0500

    ok

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -7,15 +8,19 @@ import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
+import static java.nio.file.Files.delete;
+
 public class Main {
     public static void main(String[] args) {
 
-        saveGame(new GameProgress(1,2,3,4),"C://Games//savegames//save1.dat");
-        saveGame(new GameProgress(2,3,4,5),"C://Games//savegames//save2.dat");
-        saveGame(new GameProgress(3,4,5,6),"C://Games//savegames//save3.dat");
+        saveGame(new GameProgress(1, 2, 3, 4), "C://Games//savegames//save1.dat");
+        saveGame(new GameProgress(2, 3, 4, 5), "C://Games//savegames//save2.dat");
+        saveGame(new GameProgress(3, 4, 5, 6), "C://Games//savegames//save3.dat");
 
-        zipFiles(Arrays.asList(new File("C://Games//savegames//").listFiles()),"C://Games//savegames//save.zip");
-        delFiles(Arrays.asList(new File("C://Games//savegames//").listFiles()));
+        List<File> files = Arrays.asList(new File("C://Games//savegames//").listFiles());
+
+        zipFiles(files, "C://Games//savegames//save.zip");
+        delFiles(files);
     }
 
     static void saveGame(GameProgress gameProgress, String savePath) {
@@ -27,23 +32,24 @@ public class Main {
     }
 
     static void zipFiles(List<File> file, String arch) {
-            try (ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(arch))) {
-                for (File i : file) {
-                    FileInputStream fileInputStream = new FileInputStream(i);
-                    zipOutputStream.putNextEntry(new ZipEntry(i.getName()));
-                    zipOutputStream.write(fileInputStream.readAllBytes());
-                }
-            } catch (IOException ex) {
-                System.out.println(ex.getMessage());
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(arch))) {
+            for (File i : file) {
+                FileInputStream fileInputStream = new FileInputStream(i);
+                zipOutputStream.putNextEntry(new ZipEntry(i.getName()));
+                zipOutputStream.write(fileInputStream.readAllBytes());
+                fileInputStream.close();
             }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 
 
-    static void delFiles(List<File> file){
-        for(File i : file) {
-        Pattern pattern = Pattern.compile(".dat(\\w*)");
-        Matcher matcher = pattern.matcher(i.getName());
-            if(matcher.find()) {
+    static void delFiles(List<File> file) {
+        for (File i : file) {
+            Pattern pattern = Pattern.compile(".zip(\\w*)");
+            Matcher matcher = pattern.matcher(i.getName());
+            if (!matcher.find()) {
                 if (i.delete()) {
                     System.out.println(i.toString() + " удалён");
                 } else {
@@ -53,4 +59,3 @@ public class Main {
         }
     }
 }
-
